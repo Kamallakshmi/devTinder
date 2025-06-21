e40ee6ac209387b419f3fe7d76c466edcfc7a3e4
Kamal Ramesh <kamalkannan9853@gmail.com>
2025-06-21 11:00:41 -0500

    GET, POST, DELETE

--- Learning.md
+++ Learning.md
@@ -6,3 +6,11 @@
 -- Listen to port 3000
 -- Write request handler for /test, /hello
 -- Install nodemon and update scripts inside package.json
+
+-- Initialize git
+-- .gitignore
+-- Create a remote repo on github
+-- Push all code to remote origin.
+
+-- Install Postman APP and make workspace/collection and test API call
+-- write logic to handle GET, POST, DELETE in POSTMAN
--- src/app.js
+++ src/app.js
@@ -3,10 +3,38 @@ const express = require("express");
 
 const app = express(); // creating instance of expressjs application(server)
 
+//! ORDER OF ROUTES IS MATTER
+// app.use("/", (req, res) => {
+//    this is "/" will override "/test" so even we check localhost:3000/test it will give response as "Hello from Server"
+//    this "/test" is the path now server listen only specific to "3000/test"
+//   res.send("Hello from the Server"); // this respond will send to the requesting browser and display the message on the browser as response
+// });
+
+// this will only handle GET call to /test
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Kamal", lastName: "Ramesh" });
+});
+
+app.post("/user", (req, res) => {
+  // Saving data to DB
+  res.send("Data successfully saved to the database");
+});
+app.delete("/user", (req, res) => {
+  // Saving data to DB
+  res.send("Deleted Successfully");
+});
+
+// this will match all the HTTP method API calls to /test
 // we have to handle the request which comes to server port 3000 and do response - USE METHOD
 // the function which we are passing as parameter inside use method is called REQUEST HANDLER FUNCTION
 // This is called API's we could say dummy API -- requesting the server and geting the response
 app.use("/test", (req, res) => {
+  // this "/test" is the path now server listen only specific to "3000/test"
+  res.send("Hello from the Server from test path"); // this respond will send to the requesting browser and display the message on the browser as response
+});
+
+app.use("/", (req, res) => {
+  // will the sequence matter YESSS!! now if the request localhost:3000/test server will response "Hello from the Server from test path" becasue "\test" comes before "\"
   // this "/test" is the path now server listen only specific to "3000/test"
   res.send("Hello from the Server"); // this respond will send to the requesting browser and display the message on the browser as response
 });
